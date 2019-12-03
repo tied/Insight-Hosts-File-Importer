0cf8026abeb3a2f4c8ef439c8fdbe142baa459b4
Tim Eddelbuettel <tied@me.com>
2019-12-03 18:53:25 +0100

    Mod: Predefined Structure with all needed attributes and references

--- src/main/java/me/eddelbuettel/plugins/jira/importer/manager/StructureManager.java
+++ src/main/java/me/eddelbuettel/plugins/jira/importer/manager/StructureManager.java
@@ -1,21 +1,22 @@
 package me.eddelbuettel.plugins.jira.importer.manager;
 
+import com.riadalabs.jira.plugins.insight.services.imports.common.external.DataLocator;
 import com.riadalabs.jira.plugins.insight.services.imports.common.external.model.ObjectTypeModuleExternal;
 import com.riadalabs.jira.plugins.insight.services.imports.common.external.model.external.baseversion.IconExternal;
 import com.riadalabs.jira.plugins.insight.services.imports.common.external.model.external.baseversion.InsightSchemaExternal;
 import com.riadalabs.jira.plugins.insight.services.imports.common.external.model.external.baseversion.ObjectSchemaExternal;
 import com.riadalabs.jira.plugins.insight.services.imports.common.external.model.external.baseversion.ObjectTypeExternal;
+import io.riada.StructureUtils;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 import java.io.InputStream;
 
 public class StructureManager {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
-    private static final int OBJECT_SCHEMA_ID = 1; /* Just a fake id */
+    private static final int OBJECT_SCHEMA_ID = 1;
     private int objectTypeSequenceNumber = 1;
     private int referenceTypeSequenceNumber = 1;
 
@@ -29,30 +30,41 @@ public class StructureManager {
         InsightSchemaExternal insightSchemaExternal = new InsightSchemaExternal();
         insightSchemaExternal.setObjectSchema(objectSchemaExternal);
 
-
+        /* Get Object Icon */
         IconExternal icon = getIcon("Hosts");
         insightSchemaExternal.getIcons().add(icon);
 
-        /* Root Object */
+        /* Create Root Object */
         ObjectTypeExternal rootObjectTypeExternal = new ObjectTypeModuleExternal(null, false, null);
         objectSchemaExternal.getObjectTypes().add(rootObjectTypeExternal);
+        rootObjectTypeExternal.setAbstractObjectType(true);
         rootObjectTypeExternal.setId(objectTypeSequenceNumber++);
         rootObjectTypeExternal.setName("Hosts");
         rootObjectTypeExternal.setIcon(icon);
 
-        // IP Address Child Object
+        /* Create IP Address Child Object */
         ObjectTypeExternal ipAddressObjectTypeExternal = new ObjectTypeModuleExternal(null, true, null);
         rootObjectTypeExternal.getObjectTypeChildren().add(ipAddressObjectTypeExternal);
         ipAddressObjectTypeExternal.setId(objectTypeSequenceNumber++);
         ipAddressObjectTypeExternal.setName("IP Address");
         ipAddressObjectTypeExternal.setIcon(icon);
 
-        // Domainname Child Object
-        ObjectTypeExternal domainnameObjectTypeExternal = new ObjectTypeModuleExternal(null, true, null);
-        rootObjectTypeExternal.getObjectTypeChildren().add(domainnameObjectTypeExternal);
-        domainnameObjectTypeExternal.setId(objectTypeSequenceNumber++);
-        domainnameObjectTypeExternal.setName("Domainname");
-        domainnameObjectTypeExternal.setIcon(icon);
+        /* Create Domain Name Child Object */
+        ObjectTypeExternal domainNameObjectTypeExternal = new ObjectTypeModuleExternal(null, true, null);
+        rootObjectTypeExternal.getObjectTypeChildren().add(domainNameObjectTypeExternal);
+        domainNameObjectTypeExternal.setId(objectTypeSequenceNumber++);
+        domainNameObjectTypeExternal.setName("Domain Name");
+        domainNameObjectTypeExternal.setIcon(icon);
+
+        /* Add IP Address Object Attributes */
+        DataLocator ipAddressLocator = new DataLocator("IP Address");
+        StructureUtils.addTextObjectTypeAttribute("IP Address", ipAddressObjectTypeExternal, ipAddressLocator, true, false, false, "This is the IP Address of the entry", false);
+        DataLocator domainNamesLocator = new DataLocator("Domain Names");
+        StructureUtils.addReferenceObjectTypeAttribute(insightSchemaExternal, "Domain Names", ipAddressObjectTypeExternal, domainNameObjectTypeExternal, "Relates to Domain", true, false, domainNamesLocator, false, null, "These are the related Domain Names", referenceTypeSequenceNumber++, OBJECT_SCHEMA_ID);
+
+        /* Add Domain Name Object Attributes */
+        DataLocator domainNameLocator = new DataLocator("Domain Name");
+        StructureUtils.addTextObjectTypeAttribute("Domain Name", domainNameObjectTypeExternal, domainNameLocator, true, false, false, "This is the Domain Name of the entry", false);
 
         return insightSchemaExternal;
     }
